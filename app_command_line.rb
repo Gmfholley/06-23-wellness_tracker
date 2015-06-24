114580a89129d2b513cda6aadb00b5d7ce42ea9f
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-24 11:58:01 -0500

    Adds home page
    Copies erb files from previous project to this one.

--- app_command_line.rb
+++ app_command_line.rb
@@ -18,17 +18,3 @@ require_relative 'views/method_to_call.rb'
 
 
 require_relative 'models/person.rb'
-
-get "/home" do
-  @menu = home
-  erb :menu
-end
-
-
-
-def home
-  m = Menu.new("Where would you like to go?")
-  m.add_menu_item(user_message: "Work with people", method_name: "person")
-  m
-end
-
