0b5e30e210069287d00c20d5cf3c37255a07a5f8
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-27 12:19:10 -0500

    Switches long if-else chain to switch statement
    See what I did there? eh?

--- app.rb
+++ app.rb
@@ -24,6 +24,12 @@ require_relative 'models/intensity.rb'
 require_relative 'models/exercise_type.rb'
 require_relative 'models/exercise_event.rb'
 
+
+
+require_relative 'controllers/defined_menus.rb'
+
+helpers DefinedMenus
+
 get "/home" do
   @menu = home_menu
   erb :menu
@@ -51,13 +57,15 @@ get "/:class_name/:action" do
   
   @class_name = menu_to_class_name[params["class_name"]]
   
-  if params["action"] == "update" || params["action"] == "delete"
+
+  case params["action"]
+  when "update", "delete"
     @menu = object_menu(@class_name, params["action"])
     erb :menu
-  elsif params["action"] == "show"
+  when "show"
     @menu = object_menu(@class_name, params["action"])
     erb :menu_without_links
-  elsif params["action"] == "create"
+  when "create"
     # create an object so you can get its instance variables
     @m = @class_name.new
     @foreign_key_choices = @m.foreign_key_choices
@@ -69,8 +77,7 @@ get "/:class_name/:action" do
       erb :create
     end
     
-    
-  elsif params["action"] == "submit"
+  when "submit"
     @class_name = menu_to_class_name[params["class_name"]]
     @m = @class_name.new(params["create_form"])
     @foreign_key_choices = @m.foreign_key_choices
@@ -130,50 +137,6 @@ get "/:not_listed" do
 end
 
 
-
-##############M
-# Here are my menus
-
-
-# returns a Home Menu object
-#
-# returns a Menu
-def home_menu
-  m = Menu.new("Where would you like to go?")
-  m.add_menu_item(user_message: "Work with people", method_name: "person")
-  m.add_menu_item(user_message: "Work with durations", method_name: "duration")
-  m.add_menu_item(user_message: "Work with intensities.", method_name: "intensity")
-  m.add_menu_item(user_message: "Work with exercise types.", method_name: "exercise_type")
-  m.add_menu_item(user_message: "Work with exercise events.", method_name: "exercise_event")
-  m
-end
-
-# returns a CRUD menu
-#
-# returns a Menu
-def crud_menu(class_name)
-  class_string = class_name.to_s.underscore.downcase
-  m = Menu.new("What would you like to do with #{class_string.humanize.downcase.pluralize}?")
-  m.add_menu_item(user_message: "Create a new #{class_string.humanize.downcase}.", method_name: "#{class_string}/create")
-  m.add_menu_item(user_message: "Show all #{class_string.humanize.downcase.pluralize}.", method_name: "#{class_string}/show")
-  m.add_menu_item(user_message: "Update a #{class_string.humanize.downcase}.", method_name: "#{class_string}/update")
-  m.add_menu_item(user_message: "Delete a #{class_string.humanize.downcase}.", method_name: "#{class_string}/delete")
-  m
-end
-
-
-# returns a Menu of all the class's objects
-#
-# returns a Menu
-def object_menu(class_name, action)
-  class_string = class_name.to_s.underscore.downcase
-  create_menu = Menu.new("Which #{class_string.humanize.downcase} do you want to #{action}?")
-  all = class_name.all
-  all.each_with_index do |object, x|
-    create_menu.add_menu_item({user_message: object.to_s, method_name: "#{class_string}/#{action}/#{object.id}"})
-  end
-  create_menu
-end
 
 # LookUp Hash for the menu item to the class name
 #
