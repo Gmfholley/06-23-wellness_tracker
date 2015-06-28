f0774817c6eb15a5788db00b2a76b8ca96036b36
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-28 01:24:09 -0500

    Corrects incorrect non-link html rendering
    Two options now
     - List of strings
     - Table rows of each display_field

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -14,47 +14,75 @@ module MenuController
   #
   # returns a String
   def get_list_html_for_all_menu_items(menu, with_links=true)
-    html = []  
+    html = ["<ul>"]  
     menu.menu_items.each do |item|
-      html << html_line(item, with_links)
+      html << "<li>"
+      html <<   "<a href = /#{item.method.method_name}>" if with_links
+      html <<       html_user_message_list(item)
+      html <<   "</a>" if with_links      
+      html << "</li>"
+    end
+    html << "</ul>"
+    html.join
+  end
+  
+  # returns String of table html for this menu
+  #
+  # menu        - Menu object
+  # with_links  - optional Boolean, defaults to true
+  #
+  # returns a String
+  def get_table_html_for_all_menu_items(menu, with_links=true)
+    html = ["<table>"]  
+    html << table_header
+    menu.menu_items.each do |item|
+      html << "<tr>"
+      html <<   html_table_row(item, with_links)
+      html << "</tr>"
+    end
+    html << "</table>"
+    html.join
+  end
+  
+  # returns the table header html for this class's table
+  #
+  # you have access to @class_name from original router methods
+  #
+  # returns String of html creating the table header
+  def table_header
+    header = ["<tr>"]
+    m = @class_name.new
+    m.display_fields.each do |field|
+      header << 
+      "<th>
+        #{field.humanize}
+      </th>"
+    end
+    header << "</tr>"
+    header.join
+  end
+
+  # returns a String of html forming a row in a table of this item's user_message Array
+  #
+  # item        - MenuItem object
+  # with_link   - Boolean of whether to add links
+  #
+  # returns a String
+  def html_table_row(item, with_links)
+    html = []
+    item.user_message.each do |message|
+      html << "<td>"
+      html <<   "<a href = /#{item.method.method_name}>" if with_links
+      html <<      "#{message}"
+      html <<   "</a>" if with_links
+      html << "</td>"
     end
     html.join
   end
   
-  # returns the line of html for this item
-  #
-  # item      - MenuItem type
-  # links?    - Boolean indicating if a link should surround the menu.item
-  #
-  # returns string of html
-  def html_line(item, links)
-    if links
-      "<li>
-        <a href = /#{item.method.method_name}>
-          <tr>
-            #{html_user_message_row(item.user_message)}
-          </tr>
-        </a>
-      </li>"
-    else
-      "<li>
-        <tr>
-          #{html_user_message_row(item.user_message)}
-        </tr>
-      </li>"
-    end
+  # returns all items in the user_message array joined together
+  def html_user_message_list(item)
+    item.user_message.join
   end
-
-  def html_user_message_row(user_message_array)
-    html = []
-    user_message_array.each do |item|
-      html << 
-        "<td>
-            #{item}  
-        </td>"
-    end
-    html.join
-  end
-
 
 end
